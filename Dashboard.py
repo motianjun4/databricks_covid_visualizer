0c9b14aa73a7937e3702b5c5212f1663e590802e
motianjun4 <motianjun4@gmail.com>
2022-04-12 18:17:12 -0400

    add: get real data from databricks

--- Dashboard.py
+++ Dashboard.py
@@ -1,3 +1,4 @@
+import base64
 from databricks_api import DatabricksAPI
 import os
 from dotenv import load_dotenv
@@ -15,6 +16,35 @@ class Dashboard:
         resp = self.db.jobs.export_run(latest_run_id, "DASHBOARDS")
         return resp['views'][0]['content']
 
+    def get_DBFS_data(self, path: str) -> str:
+        resp = self.db.dbfs.read(path)
+        b64 = resp['data']
+        text = base64.b64decode(b64).decode('utf-8')
+        return text
+
+    def load_json_list(self, json_text: str) -> dict:
+        import json
+        json_text = f"""[{",".join(json_text.splitlines())}]"""
+        d = json.loads(json_text)
+        return d
+    
+    def load_json_obj(self, json_text: str) -> dict:
+        import json
+        d = json.loads(json_text)
+        return d
+    
+    def get_data(self) -> dict:
+        text = self.get_DBFS_data("/FileStore/covid_date.json")
+        covid_date = self.load_json_obj(text)
+        text = self.get_DBFS_data("/FileStore/covid_summary.json")
+        covid_summary = self.load_json_obj(text)
+        text = self.get_DBFS_data("/FileStore/covid_by_region.json")
+        covid_by_region = self.load_json_list(text)
+        return {
+            "covid_date": covid_date['date'],
+            "covid_summary": covid_summary,
+            "covid_by_region": covid_by_region,
+        }
 
 if __name__ == "__main__":
     load_dotenv("secret.env")
@@ -22,5 +52,5 @@ if __name__ == "__main__":
     host=os.environ.get("DATABRICKS_HOST")
     job_id = os.environ.get("DATABRICKS_JOB_ID")
     d = Dashboard(host, token)
-    resp = d.get_latest_job_dashboard(job_id)
-    print(resp)
+    a = d.get_data()
+    print(a)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from dotenv import load_dotenv
 from Dashboard import Dashboard
 import os
@@ -16,5 +16,9 @@ d = Dashboard(host, token)
 @app.get("/dashboard", response_class=HTMLResponse)
 async def read_items():
     return d.get_latest_job_dashboard(job_id=job_id)
+
+@app.get("/covid_data", response_class=JSONResponse)
+async def get_covid_data():
+    return d.get_data()
 
 app.mount("/", StaticFiles(directory="frontend/visualizer/build", html=True), name="static")
